90bfa00191b5b044676901bce70100c39eae6dea
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2020-09-22 20:31:47 +0000

    Better variable name

--- app/app.ts
+++ app/app.ts
@@ -38,4 +38,5 @@ export class App {
         this.app.use((req, res, next) => {
             res.setHeader('Cache-Control', 'no-cache, no-store');
             res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Ac
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header('Access-Control-Allow-Methods',
