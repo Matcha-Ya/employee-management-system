80aed376d56946bcdd38296805469012a3b10d73
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2020-11-07 16:43:26 +0000

    Fix for windows

--- app/controllers/AccountsController.ts
+++ app/controllers/AccountsController.ts
@@ -1,4 +1,5 @@
 import { CustomResponse } from '../models/GeneralModels';
 import { SignUpRequestModel } from '../models/AccountsModels';
 import { AccountsDbHelper } from '../helpers/DbHelpers/AccountsDbHelper';
-import { BunyanHelper } from '../helpe
+import { BunyanHelper } from '../helpers/BunyanHelper';
+import { GlobalHelper } from '../helpers/GlobalHelper';
