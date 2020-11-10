8d523f2671b3c2043ff4975b2ea6ee5cd0028c28
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2020-11-10 22:21:26 +0000

    Update README.wiki

--- app/controllers/AccountsController.ts
+++ app/controllers/AccountsController.ts
@@ -10,4 +10,5 @@ import { ProceduresDbHelper } from '../helpers/DbHelpers/ProceduresDbHelper';
 export class AccountsController {
     private globalHelper: GlobalHelper;
     private accountDbHelper: AccountsDbHelper;
-    private proce
+    private procedureDbHelper: ProceduresDbHelper;
+    private globalConfig: any
