8691f87e1339d455f082737a9092ef478cdb43a0
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2020-12-05 18:26:52 +0000

    add presentation

--- app/controllers/StockHistoryController.ts
+++ app/controllers/StockHistoryController.ts
@@ -4,4 +4,5 @@ import { GlobalHelper } from '../helpers/GlobalHelper';
 import * as path from 'path';
 import e = require('express');
 import { request as urllibRequest, RequestOptions, HttpClientResponse } from "urllib";
-import { CompaniesDbHelper, CompaniesSingleRow, CompaniesOtherDataJson } from '../helpers/DbHel
+import { CompaniesDbHelper, CompaniesSingleRow, CompaniesOtherDataJson } from '../helpers/DbHelpers/CompaniesDbHelper';
+import { ScrapStockHistory } from '../models/StockH
