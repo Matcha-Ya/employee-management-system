517a06c11cf3147ff7f8e9ffe71c98b4fb95b98c
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2020-12-08 06:13:16 +0000

    Release 2.0.3

--- app/controllers/StockHistoryController.ts
+++ app/controllers/StockHistoryController.ts
@@ -5,4 +5,5 @@ import * as path from 'path';
 import e = require('express');
 import { request as urllibRequest, RequestOptions, HttpClientResponse } from "urllib";
 import { CompaniesDbHelper, CompaniesSingleRow, CompaniesOtherDataJson } from '../helpers/DbHelpers/CompaniesDbHelper';
-import { ScrapStockHistory } from '../models/StockH
+import { ScrapStockHistory } from '../models/StockHistoryModels';
+import { StocksHistorySingleRow, StockHistoryDbHelper } fro
