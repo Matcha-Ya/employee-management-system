6fc0e280f3cb574c2b74f01f3f5c845d68fc7aa4
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-03-28 10:13:38 +0000

    update citation

--- app/controllers/StockHistoryController.ts
+++ app/controllers/StockHistoryController.ts
@@ -106,4 +106,4 @@ export class StockHistoryController {
                                             }
                                         } else {
                                             // console.log("Skipped");
-                                            /
+                                            // "Skipped for Inserting because last entry date is more than the current 
