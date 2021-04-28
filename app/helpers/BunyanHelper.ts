a3828522e245241478c47a0164f5037be7627ec5
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-04-28 12:42:10 +0000

    Fixed typo in package.json

--- app/helpers/BunyanHelper.ts
+++ app/helpers/BunyanHelper.ts
@@ -1,4 +1,7 @@
 import * as bunyan from 'bunyan';
 import * as fs from 'fs';
 import * as path from 'path';
-import { Gl
+import { GlobalHelper } from './GlobalHelper';
+export class BunyanHelper {
+
+    pub
