958a41b84d3ca121605d047021e685357df98d23
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-04-30 19:25:22 +0000

    Remove extra code

--- app/helpers/BunyanHelper.ts
+++ app/helpers/BunyanHelper.ts
@@ -7,4 +7,4 @@ export class BunyanHelper {
     public static date = new Date().toJSON().slice(0, 10);
     // public static logsDirWithDate = path.resolve(__dirname, "..", "..", "logs", BunyanHelper.date);
     public static logDir = new GlobalHelper().getConfig("global")["settings"]["log_dir"];
-    p
+    public static logsDirWithDate = path.resolve(BunyanHelper.logDir, "logs"
