f3892527cbda87955ab27a2a3089219375706f94
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-05-08 11:41:01 +0000

    counter

--- app/helpers/BunyanHelper.ts
+++ app/helpers/BunyanHelper.ts
@@ -7,4 +7,7 @@ export class BunyanHelper {
     public static date = new Date().toJSON().slice(0, 10);
     // public static logsDirWithDate = path.resolve(__dirname, "..", "..", "logs", BunyanHelper.date);
     public static logDir = new GlobalHelper().getConfig("global")["settings"]["log_dir"];
-    public static logsDirWithDate = path.resolve(BunyanHelper.logDir, "logs"
+    public static logsDirWithDate = path.resolve(BunyanHelper.logDir, "logs", BunyanHelper.date);
+
+    /**
+     * creates folder for logs if not ava
