f43ec401c8be219314c971276c552859ea864c57
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-08-17 15:41:59 +0000

    Code quality

--- app/helpers/DbHelpers/AccountsDbHelper.ts
+++ app/helpers/DbHelpers/AccountsDbHelper.ts
@@ -1,4 +1,7 @@
 import { MySQLHelper } from '../MySQLHelper';
 import { GlobalHelper } from '../GlobalHelper';
 import { BunyanHelper } from "../BunyanHelper";
-import { DbHelperReturn } from '../../models/GeneralMo
+import { DbHelperReturn } from '../../models/GeneralModels';
+
+export class AccountsDbHelper {
+    private globalHelper: Globa
