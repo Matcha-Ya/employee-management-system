79bd2bcd02001defe442c4e66a2705fb93b92f43
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-09-04 22:06:06 +0000

    Add more logging.

--- app/helpers/DbHelpers/AccountsDbHelper.ts
+++ app/helpers/DbHelpers/AccountsDbHelper.ts
@@ -43,4 +43,7 @@ export class AccountsDbHelper {
                 + "?,"
                 + "?,"
                 + "?,"
-            
+                + "?,"
+                + "?,"
+                + "?,"
+   
