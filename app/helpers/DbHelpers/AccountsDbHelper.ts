58483865568f61bd70817d1001def081ba6cf519
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-09-05 14:08:32 +0000

    courses

--- app/helpers/DbHelpers/AccountsDbHelper.ts
+++ app/helpers/DbHelpers/AccountsDbHelper.ts
@@ -46,4 +46,7 @@ export class AccountsDbHelper {
                 + "?,"
                 + "?,"
                 + "?,"
-   
+                + "?,"
+                + "?,"
+                + "?"
+        
