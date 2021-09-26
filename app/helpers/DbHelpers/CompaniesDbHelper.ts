53fb7851dcfe4f19a24ad3f9015e638423682362
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-09-26 11:31:49 +0000

    update a post

--- app/helpers/DbHelpers/CompaniesDbHelper.ts
+++ app/helpers/DbHelpers/CompaniesDbHelper.ts
@@ -42,3 +42,68 @@ export class CompaniesDbHelper {
                 + "?,"
                 + "?,"
                 + "?,"
+                + "?"
+                + ")";
+
+            if (this.globalConfig["settings"]["log_verbose"]) {
+                BunyanHelper.activityLogger.info(sqlQuery);
+            }
+
+            var results = await MySQLHelper.executeQuery(sqlQuery, [
+                companySingleRow.company_symbol_code?.toUpperCase(),
+                companySingleRow.market_exchange?.toUpperCase(),
+                companySingleRow.company_name,
+                companySingleRow.company_sector?.toUpperCase(),
+                companySingleRow.isin?.toUpperCase(),
+                companySingleRow.company_mf_sector,
+                JSON.stringify(companySingleRow.other_data),
+                companySingleRow.dead == null ? 0 : companySingleRow.dead
+            ]);
+
+            if (results[0].affectedRows > 0) {
+                insertCompanyReturn.isError = false;
+            }
+            else {
+                insertCompanyReturn.isError = true;
+            }
+
+            insertCompanyReturn.result = results;
+
+        } catch (error) {
+            BunyanHelper.errorLogger.error(error);
+            console.error(error);
+            insertCompanyReturn.isError = true;
+            insertCompanyReturn.result = error;
+        } finally {
+            return insertCompanyReturn;
+        }
+
+    }
+
+
+    public async getCompanyBy(condition: "id" | "company_symbol_code", conditionValue: string, dead: 0 | 1): Promise<DbHelperReturn> {
+        var getUserByReturn = new DbHelperReturn();
+        try {            
+            let sqlQuery: string = "";
+
+            switch (condition) {
+                case "id":
+                    sqlQuery = "SELECT * FROM `companies` where `companies`.`id` = ? and `companies`.`dead` = ?";
+                    break;
+                case "company_symbol_code":
+                    sqlQuery = "SELECT * FROM `companies` where `companies`.`company_symbol_code` = ? and `companies`.`dead` = ?";
+                    break;
+                default:
+                    throw "Invalid condition";
+            }
+
+            var results = await MySQLHelper.executeQuery(sqlQuery, [conditionValue, dead]);
+
+            if (results[0].length > 0) {
+                getUserByReturn.isError = false;
+            }
+            else {
+                getUserByReturn.isError = true;
+            }
+
+            getUserByReturn.result = results;
