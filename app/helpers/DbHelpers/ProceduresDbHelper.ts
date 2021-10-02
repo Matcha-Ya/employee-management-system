8bc523257938ddeeceeb9de1f319243a0f003358
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-10-02 21:47:00 +0000

    Update quick_start.rst

--- app/helpers/DbHelpers/ProceduresDbHelper.ts
+++ app/helpers/DbHelpers/ProceduresDbHelper.ts
@@ -6,4 +6,4 @@ import { DbHelperReturn, DatatableRequestModel } from '../../models/GeneralModel
 
 /**
  * Every procedure of our database should be called from here.
- * So that any parameter changes and procedure response changes can be don
+ * So that any parameter changes and procedure response changes can be done directly inside here rather than look to into each and every database helper
