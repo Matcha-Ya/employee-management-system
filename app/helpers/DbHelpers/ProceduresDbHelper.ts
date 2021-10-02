23289e5acf87b12493d321d25a3f1740d4fe821c
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-10-02 15:17:44 +0000

    tag fix

--- app/helpers/DbHelpers/ProceduresDbHelper.ts
+++ app/helpers/DbHelpers/ProceduresDbHelper.ts
@@ -2,4 +2,7 @@ import { MySQLHelper } from '../MySQLHelper';
 import { GlobalHelper } from '../GlobalHelper';
 import { } from "promise";
 import { BunyanHelper } from "../BunyanHelper";
-import { DbHelperReturn, DatatableRequestModel } from '../../models/Gen
+import { DbHelperReturn, DatatableRequestModel } from '../../models/GeneralModels';
+
+/**
+ * Every procedure of our database should be called 
