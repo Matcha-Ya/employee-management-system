7282e1c50597e06b59c6f8289b9a10d5782a0d2c
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-10-03 13:28:34 +0000

    Update contacts.html

--- app/helpers/DbHelpers/ProceduresDbHelper.ts
+++ app/helpers/DbHelpers/ProceduresDbHelper.ts
@@ -8,4 +8,4 @@ import { DbHelperReturn, DatatableRequestModel } from '../../models/GeneralModel
  * Every procedure of our database should be called from here.
  * So that any parameter changes and procedure response changes can be done directly inside here rather than look to into each and every database helper.
  *  ## Why single procedure db  helper? 
- *  - Its because in MySQL Procedures are
+ *  - Its because in MySQL Procedures are also all in one folder, they are not like individual tables related like 
