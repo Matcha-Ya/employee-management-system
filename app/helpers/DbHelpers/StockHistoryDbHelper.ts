6941e56a8e4caeff20357dfd1f0b9bc0c7c81f57
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2021-11-27 12:15:37 +0000

    fixing setup

--- app/helpers/DbHelpers/StockHistoryDbHelper.ts
+++ app/helpers/DbHelpers/StockHistoryDbHelper.ts
@@ -68,3 +68,69 @@ export class StockHistoryDbHelper {
                 stockHistorySingleRow.per,
                 stockHistorySingleRow.volume,
                 stockHistorySingleRow.dead == null ? 0 : stockHistorySingleRow.dead
+            ]);
+
+            if (results[0].affectedRows > 0) {
+                insertStockHistoryReturn.isError = false;
+            }
+            else {
+                insertStockHistoryReturn.isError = true;
+            }
+
+            insertStockHistoryReturn.result = results;
+
+        } catch (error) {
+            BunyanHelper.errorLogger.error(error);
+            console.error(error);
+            insertStockHistoryReturn.isError = true;
+            insertStockHistoryReturn.result = error;
+        } finally {
+            return insertStockHistoryReturn;
+        }
+
+    }
+
+    public async getStockHistory(companyId: number, orderDir: "asc" | "desc", limit: number): Promise<DbHelperReturn> {
+        var getUserByReturn = new DbHelperReturn();
+        try {
+
+            let sqlQuery: string = "select * from `stocks_history` where `company_id` = ? and `dead` = ? order by `on_date` " + orderDir + " limit " + limit;
+
+            var results = await MySQLHelper.executeQuery(sqlQuery, [companyId, 0]);
+
+            if (results[0].length > 0) {
+                getUserByReturn.isError = false;
+            }
+            else {
+                getUserByReturn.isError = true;
+            }
+
+            getUserByReturn.result = results;
+
+        } catch (error) {
+            BunyanHelper.errorLogger.error(error);
+            console.error(error);
+            getUserByReturn.isError = true;
+            getUserByReturn.result = error;
+        } finally {
+            return getUserByReturn;
+        }
+
+    }
+
+
+}
+
+
+export class StocksHistorySingleRow {
+    public id?: number;
+    public company_id?: number;
+    public on_date?: Date;
+    public open_price?: number;
+    public high_price?: number;
+    public low_price?: number;
+    public close_price?: number;
+    public per?: number;
+    public volume?: number;
+    public dead?: number;
+}
