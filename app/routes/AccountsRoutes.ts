fdda3816afbd20c9f3a7575f63b9ae855bbf311d
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-02-13 19:55:41 +0000

    Fix rubocop offences

--- app/routes/AccountsRoutes.ts
+++ app/routes/AccountsRoutes.ts
@@ -1,3 +1,5 @@
 import { Request, Response } from 'express';
 import { AccountsController } from '../controllers/AccountsController';
-import { SignUpRequestModel } from '../models/AccountsMo
+import { SignUpRequestModel } from '../models/AccountsModels';
+import { GlobalHelper } from '../helpers/GlobalHelper';
+import { Custom
