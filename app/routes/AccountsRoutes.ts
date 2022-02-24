9c1d80fd13d55ab84dc9c14cb6cb2befad075bf5
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-02-24 07:41:09 +0000

    initial add

--- app/routes/AccountsRoutes.ts
+++ app/routes/AccountsRoutes.ts
@@ -2,4 +2,7 @@ import { Request, Response } from 'express';
 import { AccountsController } from '../controllers/AccountsController';
 import { SignUpRequestModel } from '../models/AccountsModels';
 import { GlobalHelper } from '../helpers/GlobalHelper';
-import { Custom
+import { CustomResponse } from '../models/GeneralModels';
+/**
+ * Routes for Account Controller
+ 
