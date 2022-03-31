8ba5ad7d34e7aab35cb37f9aaa05e73dfd36f897
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-03-31 07:03:38 +0000

    ci skip

--- app/routes/AccountsRoutes.ts
+++ app/routes/AccountsRoutes.ts
@@ -35,4 +35,5 @@ export class AccountsRoutes {
                     }
                 } catch (error) {
                     console.log(error);
-                    custo
+                    customResponse.error_code = 500;
+                    customResponse.error_messag
