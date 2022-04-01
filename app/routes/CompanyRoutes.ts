0d935c59582d91a439a3b8e502e245257a0c5d87
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-04-01 12:57:10 +0000

    update validation

--- app/routes/CompanyRoutes.ts
+++ app/routes/CompanyRoutes.ts
@@ -37,3 +37,31 @@ export class CompanyRoutes {
                     customResponse.error_messages = "Something went wrong";
                 }
                 finally {
+                    res.send(customResponse);
+                }
+            });
+
+        this.router.route('/scrap_stock_history')
+            .post(async (req: Request, res: Response) => {
+                var customResponse: CustomResponse = new CustomResponse();
+
+                try {
+                    var reqBody = req.body as ScrapCompanyDetails;
+                    customResponse = await this.companyController.scrapCompanyDetails(reqBody);
+                    if (customResponse.error_code != 200 && customResponse.error_code != undefined) {
+                        res.status(customResponse.error_code);
+                    } else {
+                        res.status(200);
+                    }
+                } catch (error) {
+                    console.log(error);
+                    customResponse.error_code = 500;
+                    customResponse.error_messages = "Something went wrong";
+                }
+                finally {
+                    res.send(customResponse);
+                }
+            });
+
+    }
+}
