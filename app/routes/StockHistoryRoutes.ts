4834453e1617d229602f02eec50856e39220e263
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-04-04 22:33:29 +0000

    Fixes #91

--- app/routes/StockHistoryRoutes.ts
+++ app/routes/StockHistoryRoutes.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
 import { GlobalHelper } from '../helpers/GlobalHelper';
 import { CustomResponse } from '../models/GeneralModels';
-import { StockHistoryControll
+import { StockHistoryController } from '../controllers/StockHistoryController';
+import { Scra
