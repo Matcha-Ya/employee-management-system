2bbac17ad8724136c821d7bd4b137b5d1ae92441
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-04-04 10:22:30 +0000

    Updated Github Pages page

--- app/routes/StockHistoryRoutes.ts
+++ app/routes/StockHistoryRoutes.ts
@@ -1,2 +1,4 @@
 import { Request, Response } from 'express';
-import { GlobalHelper } from '../helpers/GlobalHelper
+import { GlobalHelper } from '../helpers/GlobalHelper';
+import { CustomResponse } from '../models/GeneralModels';
+import { StockHistoryControll
