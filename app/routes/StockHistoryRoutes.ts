b31bd9acf8132a94602cc54af42b8b20530fffc9
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-04-22 12:41:32 +0000

    USB: serial: option: add support for Quectel UC20

--- app/routes/StockHistoryRoutes.ts
+++ app/routes/StockHistoryRoutes.ts
@@ -25,4 +25,5 @@ export class StockHistoryRoutes {
                 try {
                     var reqBody = req.body as ScrapStockHistory;
                     customResponse = await this.stockHistoryController.scrapStockHistory(reqBody);
-                    if (customResponse.error_code 
+                    if (customResponse.error_code != 200 && customResponse.error_code != undefined) {
+                
