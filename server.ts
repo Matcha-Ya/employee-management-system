fea2e32b1b73e70164c7c18719743fd9c31c1405
Matcha-Ya <108456801+Matcha-Ya@users.noreply.github.com>
2022-08-28 13:37:13 +0000

    refact, please ignore

--- server.ts
+++ server.ts
@@ -1,3 +1,7 @@
 import { App } from './app/app';
 import { BunyanHelper } from './app/helpers/BunyanHelper';
 import { GlobalHelper } from './app/helpers/GlobalHelper';
+
+var globalHelper = new GlobalHelper();
+
+var globalConfig = globalHelpe
